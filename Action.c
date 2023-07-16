d499f65ba2b969fd9524da46772e16a55fed1b90
Naresh Personal Email <nareshganugu@gmail.com>
2023-07-16 16:37:25 -0400

    Version-1

---
+++ Action.c
@@ -0,0 +1,17 @@
+Action()
+{
+	
+	
+	web_custom_request("web_custom_request",
+		"URL=www.cars.com",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+
+	
+	
+	return 0;
+}
